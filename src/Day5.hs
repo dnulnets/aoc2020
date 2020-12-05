f34c2dea3d53b335be39f05bf89de8b7f5734441
dnulnets <tomas.stenlund@telia.com>
2020-12-05 22:14:41 +0100

    Reorganized Day5

--- src/Day5.hs
+++ src/Day5.hs
@@ -9,32 +9,21 @@ allSeats = fromList [0..127*8+7]
 
 seat::[Char]->Int
 seat s = binary (take 7 s)*8 + binary (drop 7 s)
-
-binary::[Char]->Int
-binary = foldl digit 0
-    where
-        digit b 'B' = b * 2 + 1
-        digit b 'F' = b * 2
-        digit b 'L' = b * 2
-        digit b 'R' = b * 2 + 1
-
--- Return with the list of free sats on the plane
---freeSeats :: [Int] -> [Int]
---freeSeats = toList . difference allSeats . fromList
-
--- Return with a list of the neighbours given a seat
---neighbours::[Int] -> [Int]
---neighbours = concatMap neighbour
---    where
---        neighbour n = [n-1, n+1]
+    where 
+        binary = foldl digit 0
+            where
+                digit b 'B' = b * 2 + 1
+                digit b 'F' = b * 2
+                digit b 'L' = b * 2
+                digit b 'R' = b * 2 + 1
 
 -- Second part of day 5
 problemB::Problem [Int] [Int]
 problemB = Problem {parse = map seat . lines, solve = findSeat}
     where
-        findSeat l = toList (difference (fromList (freeSeats l)) (neighbours (freeSeats l)))
+        findSeat l = toList $ difference (fromList $ freeSeats l) (neighbours $ freeSeats l)
         freeSeats = toList . difference allSeats . fromList
-        neighbours l = fromList $ concatMap neighbour l
+        neighbours = fromList . concatMap neighbour
             where
                 neighbour n = [n-1, n+1]
 
